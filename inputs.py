f920a032d3091c4ee3366edc773a72bf48cb8a7f
pingyujc <pingyujc@gmail.com>
2023-01-13 01:57:03 -0800

    inputs improvements

--- inputs.py
+++ inputs.py
@@ -6,12 +6,16 @@ Collect:
         - who is spliitng this?
 """
 # Data structure like this:
-# jon: ( [100, [a,b,c]], [200, [b]], [50, [c]] )
+# [ 
+# [jon, [100, [a,b,c]], [200, [b]], [50, [c]] ], 
+# [jc, [20, [a]]] 
+# ]
 # jon paid total of 3 bills
 
 
 def CollectInput():
     name = ""
+    bill_ppl = []
     amount = 0.0
     paid_person = []
 
@@ -24,6 +28,7 @@ def CollectInput():
 
 
     amount = float(input("Enter bill amount: \n"))
+    bill_ppl.append(amount)
     #print(f"{name} paid {amount}")
 
     while True:
@@ -33,8 +38,37 @@ def CollectInput():
         else:
             paid_person.append(person)
     #print(paid_person)
+    bill_ppl.append(paid_person)
 
-    print(f"{name} paid {amount} with {paid_person}")
+    #print(f"{name} paid {amount} with {paid_person}")
+    #print(f"{name} and {bill_ppl}")
+    return name, bill_ppl
+
+
+def ifExist(database, name):
+    for i in range(len(database)):
+        if name == database[i][0]:
+            return True
+    return False
+
+def findIndex(database, name):
+    for i in range(len(database)):
+        if name == database[i][0]:
+            return i
+    return -1
+
+
+
+def AddInput(database, name, bill_ppl):
+    # this function will take 3 parameter:
+    # a name and a list containing a single bill and ppl spliiting it, and the table it is adding to
+    if ifExist(database=database, name=name) == False:
+        temp = [name, bill_ppl]
+        database.append(temp)
+    else:
+        database[findIndex(database=database, name=name)].append(bill_ppl)
+        
+
 
 
     
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # The file for running the calculation after collecting the inputs
-from inputs import CollectInput
+import inputs
+"""from inputs import CollectInput
+from inputs import ifExist
+from inputs import findIndex
+from inputs import AddInput"""
+
+name, info = inputs.CollectInput()
 
 
-CollectInput()
+db = []
+inputs.AddInput(db, name, info)
+name, info = inputs.CollectInput()
+inputs.AddInput(db, name, info)
+
+
+print(db)
